e3fe9f3e851bc1de1800b4c14dcdd7069b622ffb
Germán Martínez <germaga92@gmail.com>
2022-10-01 21:20:15 +0200

    Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import { BrowserRouter, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import ConnectSafe from 'src/components/ConnectSafe'
+import Header from 'src/components/Header'
+import Home from 'src/pages/Home'
+import { HOME_PATHNAME } from './routes/routes'
 
-function App() {
+const App = () => {
+  const isAnIframe = window.self !== window.top
+  console.log({isAnIframe})
   return (
     <BrowserRouter>
-      <Routes>
-      </Routes>
+      <Header />
+      {!isAnIframe ? (
+        <ConnectSafe />
+      ) : (
+        <Routes>
+          <Route path={HOME_PATHNAME} element={<Home />} />
+        </Routes>
+      )}
     </BrowserRouter>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,23 @@
-import { CssBaseline } from '@mui/material'
+import { SafeProvider } from '@gnosis.pm/safe-apps-react-sdk'
+import { theme } from '@gnosis.pm/safe-react-components'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from 'src/App'
-import Providers from 'src/components/providers/Providers'
+import StoreProvider from 'src/store'
+import GlobalStyles from 'src/theme/global'
+import { ThemeProvider } from 'styled-components'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
   <React.StrictMode>
-    <Providers>
-      <>
-        <CssBaseline />
-        <App />
-      </>
-    </Providers>
+    <GlobalStyles />
+    <ThemeProvider theme={theme}>
+      <SafeProvider>
+        <StoreProvider>
+          <App />
+        </StoreProvider>
+      </SafeProvider>
+    </ThemeProvider>
   </React.StrictMode>
 )
